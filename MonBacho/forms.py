e77826dc770a105a29bd1769e878f1c4ae816ad5
EDOU EDOU Vanney Joinel <ejoinel@yahoo.fr>
2014-12-21 06:58:37 +0100

    La page de login commence a fonctionner

--- MonBacho/forms.py
+++ MonBacho/forms.py
@@ -3,8 +3,19 @@
 from django import forms
 
 import FORM_PROPERTIES
+import ERROR_TXT
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField(label=FORM_PROPERTIES.FORM_EMAIL)
     password = forms.CharField(label=FORM_PROPERTIES.FORM_PASSWORD, widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super (LoginForm, self).clean()
+        email = cleaned_data.get("email")
+        password = cleaned_data.get("password")
+        # Vérifie que les deux champs sont valides
+        if email and password:
+            if password != "sesame" or email != "ejoinel@yahoo.fr":
+                raise forms.ValidationError(ERROR_TXT.ERROR_EMAIL_PASSWORD_BAD)
+        return cleaned_data
