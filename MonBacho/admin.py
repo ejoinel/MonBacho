2fd2450c0c0ff806d038583d994b87a676f8d46a
EDOU EDOU Vanney Joinel <ejoinel@yahoo.fr>
2015-02-16 00:59:20 +0100

    Modifications de la BD

--- MonBacho/admin.py
+++ MonBacho/admin.py
@@ -1,12 +1,12 @@
 #-*- coding: utf-8 -*-
 
 from django.contrib import admin
-from MonBacho.models import classtypes, schools, classgrades, subjects, courses, examens, concern
+from MonBacho.models import course, examen, concern, subject, personne, student, professor
 
-admin.site.register(schools)
-admin.site.register(classgrades)
-admin.site.register(classtypes)
-admin.site.register(courses)
-admin.site.register(subjects)
-admin.site.register(examens)
+admin.site.register(course)
+admin.site.register(personne)
+admin.site.register(student)
+admin.site.register(professor)
+admin.site.register(subject)
+admin.site.register(examen)
 admin.site.register(concern)
--- MonBacho/models.py
+++ MonBacho/models.py
@@ -3,19 +3,29 @@
 from django.db import models
 
 
-
-class classtypes (models.Model):
-
-    class Meta:
-        db_table = 'classtype'
-    name = models.CharField(max_length=30)
-
-    def __unicode__(self):
-        return self.name
+STUDENT_TYPE_CHOICES = (
+    ('0', 'Terminale'),
+    ('1', 'Troisième'),
+)
 
 
 
-class subjects (models.Model):
+PERSON_SEX_CHOISE = (
+    ('0', 'Mr'),
+    ('1', 'Mme'),
+    ('2', 'Mlle')
+)
+
+
+
+SCHOOL_TYPE_CHOICES = (
+    ('0', 'Lycée Leon MBA'),
+    ('1', 'Collège Béssieux'),
+)
+
+
+
+class subject (models.Model):
 
     class Meta:
         db_table = 'subject'
@@ -26,39 +36,74 @@ class subjects (models.Model):
 
 
 
-class courses (models.Model):
+class personne(models.Model):
+
+    class Meta:
+        db_table = 'person'
+
+    sex = models.IntegerField(choices=PERSON_SEX_CHOISE, default=0)
+    name = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    mail = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.nickname + "(" + self.nickname + ")"
+
+
+
+class student(personne):
+
+    class Meta:
+        db_table = 'student'
+
+    grade = models.IntegerField(choices=STUDENT_TYPE_CHOICES, default=0)
+
+    def __unicode__(self):
+        return self.nickname + "(" + self.nickname + ")"
+
+
+
+class professor(personne):
+
+    class Meta:
+        db_table = 'professor'
+
+    school = models.IntegerField(choices=SCHOOL_TYPE_CHOICES, default=0)
+    school = models.IntegerField(choices=SCHOOL_TYPE_CHOICES, default=0)
+    function = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.sex + " " + self.nickname + "(" + self.nickname + ")"
+
+
+
+
+class course (models.Model):
 
     class Meta:
         db_table = 'course'
     name = models.CharField(max_length=30)
-    subject = models.ForeignKey(subjects, null=True, blank=True, default=None)
+    subject = models.ForeignKey(subject, null=True, blank=True, default=None)
 
     def __unicode__(self):
         return self.name
 
 
 
-class examens (models.Model):
+class examen (models.Model):
 
     class Meta:
         db_table = 'examen'
     name = models.CharField(max_length=30)
     is_real_examen = models.BooleanField(default=True)
-    subject = models.ManyToManyField('subjects', through='concern', related_name='subject')
+    subject = models.ManyToManyField('subject', through='concern', related_name='subject')
 
     def __unicode__(self):
         return self.name + " " + self.is_real_examen
-
-
-
-class schools (models.Model):
-
-    class Meta:
-        db_table = 'school'
-    name = models.CharField(max_length=30)
-
-    def __unicode__(self):
-        return self.name
 
 
 
@@ -68,8 +113,9 @@ class classgrades (models.Model):
         db_table = 'classgrade'
     name = models.CharField(max_length=30)
     creation_date = models.DateField()
-    school = models.ForeignKey(schools, null=True, blank=True, default=None)
-    examen = models.ManyToManyField('examens', through='concern', related_name='examens')
+    school = models.IntegerField(choices=SCHOOL_TYPE_CHOICES, default=0)
+    grade = models.IntegerField(choices=STUDENT_TYPE_CHOICES, default=0)
+    examen = models.ManyToManyField('examen', through='concern', related_name='examen')
 
     def __unicode__(self):
         return self.name
@@ -80,11 +126,12 @@ class concern (models.Model):
 
     class Meta:
         db_table = 'concern'
-    examdate = models.DateField()
-    subject = models.ForeignKey(subjects, related_name='PassExam')
-    examen = models.ForeignKey(examens, related_name='PassExam')
+    exam_date = models.DateField()
+    subject = models.ForeignKey(subject, related_name='PassExam')
+    examen = models.ForeignKey(examen, related_name='PassExam')
     classgrade = models.ForeignKey(classgrades, related_name='PassExam')
 
     def __unicode__(self):
         return self.pass_date + " - " + self.examen.name + " (" + self.classgrade.name + "," + self.subject.name + ")"
+
 
