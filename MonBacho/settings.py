fac1a54c73e9f441c7685e0f65879a4b5580326a
ejoinel <ejoinel@yahoo.fr>
2015-10-23 01:15:40 +0200

    crispy_forms
    ajout de crispy_forms dans les formulaires

--- MonBacho/settings.py
+++ MonBacho/settings.py
@@ -36,6 +36,8 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
 
 # Application definition
 
@@ -46,6 +48,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'MonBacho',
  )
 
